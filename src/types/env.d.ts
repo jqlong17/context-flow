ef3f645175068656ea2479090deffa62058c4dec
ruska <longjiaqiuu@gmail.com>
2025-02-23 16:44:48 +0800

    fix: update environment variables configuration

---
+++ src/types/env.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_SUPABASE_URL: string;
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+    NEXT_PUBLIC_ZHIPU_API_KEY: string;
+    NEXT_PUBLIC_ZHIPU_API_URL: string;
+  }
+} 
